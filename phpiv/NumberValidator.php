90689ef0bbcbc6199383122ae98210242ce6f4dc
Daniel Abrajan C <seorc.d@gmail.com>
2015-05-28 19:29:54 +0000

    Add min-max validators to NumverValidator.

--- phpiv/NumberValidator.php
+++ phpiv/NumberValidator.php
@@ -12,6 +12,34 @@ class NumberValidator extends Validator {
 	protected function clean() {
 		return (int) $this->value;
 	}
+
+	public function min($min) {
+		if(!is_numeric($min)) {
+			throw InvalidArgumentException('You must pass a number to this method');
+		}
+		$this->v['min'] = $min;	
+		return $this;
+	}
+
+	public function max($max) {
+		if(!is_numeric($max)) {
+			throw new InvalidArgumentException('You must pass a number to this method');
+		}
+		$this->v['max'] = $max;	
+		return $this;
+	}
+
+	public function minCheck(array $data) {
+		if(!$this->isEmpty && $this->value < $this->v['min']) {
+			return "No puede ser menor de {$this->v['min']}";
+		}
+	}
+
+	public function maxCheck(array $data) {
+		if(!$this->isEmpty && $this->value > $this->v['max']) {
+			return "No puede ser mayor de {$this->v['max']}";
+		}
+	}
 }
 
 ?>
--- tests/NumberValidatorTest.php
+++ tests/NumberValidatorTest.php
@@ -1,11 +1,96 @@
 <?php
 
 class NumberValidatorTest extends PHPUnit_Framework_TestCase {
+
+	public function testChainableMethodsIndeedChain() {
+		$v = new NumberValidator('numval');
+		$this->assertEquals($v->min(2), $v);
+		$this->assertEquals($v->max(3), $v);
+	}
+
 	public function testValueIsCleanedToNumber() {
 		$v = new NumberValidator('numval');
 		$v->check(array('numval' => '01'));
 		$this->assertThat($v->cleanedValue, $this->isType('int'));
 	}
+
+	/**
+	 * @dataProvider valuesProvider
+	 */
+	public function testMinIsWorking($input) {
+		$m = 3;
+		$v = new NumberValidator('numval');
+		$v->min($m);
+		if($input < $m) {
+			$this->setExpectedException('ValidationError');
+		}
+		$v->check(array('numval' => $input));
+	}
+
+	/**
+	 * @dataProvider valuesProvider
+	 */
+	public function testMaxIsWorking($input) {
+		$m = 8;
+		$v = new NumberValidator('numval');
+		$v->max($m);
+		if($input > $m) {
+			$this->setExpectedException('ValidationError');
+		}
+		$v->check(array('numval' => $input));
+	}
+
+	public function invalidMinMaxProvider() {
+		return array(
+			// Invalid inputs.
+			array('i9', false),
+			array('sofij', false),
+			array('true', false),
+			array(false, false),
+			// Valid inputs.
+			array('1', true),
+			array(8, true),
+			array(8, true),
+		);
+	}
+
+	public function valuesProvider() {
+		return array(
+			array(1),
+			array(2),
+			array(3),
+			array(4),
+			array(5),
+			array(10),
+			array(20),
+			array(30),
+			array(40),
+			array(50),
+		);
+	}
+
+	/**
+	 * @dataProvider invalidMinMaxProvider
+	 */
+	public function testExceptionOnWrongMin($min, $valid) {
+		$v = new NumberValidator('val');
+		if(!$valid) {
+			$this->setExpectedException('InvalidArgumentException');
+		}
+		$v->max($min);
+	}
+
+	/**
+	 * @dataProvider invalidMinMaxProvider
+	 */
+	public function testExceptionOnWrongMax($max, $valid) {
+		$v = new NumberValidator('val');
+		if(!$valid) {
+			$this->setExpectedException('InvalidArgumentException');
+		}
+		$v->max($max);
+	}
+
 }
 
 ?>
