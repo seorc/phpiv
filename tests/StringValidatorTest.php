f941881e5cd7d43c202ae3d319492b2210608139
Daniel Abraján C <seorc.d@gmail.com>
2016-03-10 00:07:46 -0600

    A test that length(min, max) validation is working.

--- tests/StringValidatorTest.php
+++ tests/StringValidatorTest.php
@@ -16,7 +16,26 @@ class StringValidatorTest extends PHPUnit_Framework_TestCase {
     public function testLengtSingleParamMeansFixedLenght($value, $valid) {
         $v = new StringValidator('tested');
         $v->length(4);
+        if(!$valid) $this->setExpectedException('ValidationError');
+        $v->check(['tested' => $value]);
+    }
 
+
+    public function rangedLengthProvider() {
+        return [
+            ['1234', true],
+            ['123', false],
+            ['123456', true],
+            ['1234567', false],
+        ];
+    }
+
+    /**
+     * @dataProvider rangedLengthProvider
+     */
+    public function testLengthCanValidateRanges($value, $valid) {
+        $v = new StringValidator('tested');
+        $v->length(4, 6);
         if(!$valid) $this->setExpectedException('ValidationError');
         $v->check(['tested' => $value]);
     }
